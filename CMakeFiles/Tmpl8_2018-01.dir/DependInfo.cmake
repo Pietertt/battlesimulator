639f3cbd58e985cdeecac1be680bbf028729dc46
Pieter Boersma <pieterboersma20@gmail.com>
2020-11-14 09:53:03 +0100

    Working code

---
+++ CMakeFiles/Tmpl8_2018-01.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/pieterboersma/Desktop/battlesimulator/explosion.cpp" "/Users/pieterboersma/Desktop/battlesimulator/CMakeFiles/Tmpl8_2018-01.dir/explosion.cpp.o"
+  "/Users/pieterboersma/Desktop/battlesimulator/game.cpp" "/Users/pieterboersma/Desktop/battlesimulator/CMakeFiles/Tmpl8_2018-01.dir/game.cpp.o"
+  "/Users/pieterboersma/Desktop/battlesimulator/particle_beam.cpp" "/Users/pieterboersma/Desktop/battlesimulator/CMakeFiles/Tmpl8_2018-01.dir/particle_beam.cpp.o"
+  "/Users/pieterboersma/Desktop/battlesimulator/rocket.cpp" "/Users/pieterboersma/Desktop/battlesimulator/CMakeFiles/Tmpl8_2018-01.dir/rocket.cpp.o"
+  "/Users/pieterboersma/Desktop/battlesimulator/smoke.cpp" "/Users/pieterboersma/Desktop/battlesimulator/CMakeFiles/Tmpl8_2018-01.dir/smoke.cpp.o"
+  "/Users/pieterboersma/Desktop/battlesimulator/surface.cpp" "/Users/pieterboersma/Desktop/battlesimulator/CMakeFiles/Tmpl8_2018-01.dir/surface.cpp.o"
+  "/Users/pieterboersma/Desktop/battlesimulator/tank.cpp" "/Users/pieterboersma/Desktop/battlesimulator/CMakeFiles/Tmpl8_2018-01.dir/tank.cpp.o"
+  "/Users/pieterboersma/Desktop/battlesimulator/template.cpp" "/Users/pieterboersma/Desktop/battlesimulator/CMakeFiles/Tmpl8_2018-01.dir/template.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/System/Library/Frameworks/OpenGL.framework"
+  "/usr/local/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Tmpl8_2018-01.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tmpl8_2018-01.dir/explosion.cpp.o"
+  "CMakeFiles/Tmpl8_2018-01.dir/game.cpp.o"
+  "CMakeFiles/Tmpl8_2018-01.dir/particle_beam.cpp.o"
+  "CMakeFiles/Tmpl8_2018-01.dir/rocket.cpp.o"
+  "CMakeFiles/Tmpl8_2018-01.dir/smoke.cpp.o"
+  "CMakeFiles/Tmpl8_2018-01.dir/surface.cpp.o"
+  "CMakeFiles/Tmpl8_2018-01.dir/tank.cpp.o"
+  "CMakeFiles/Tmpl8_2018-01.dir/template.cpp.o"
+  "Tmpl8_2018-01"
+  "Tmpl8_2018-01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tmpl8_2018-01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
