4c8860f535fa05959f73f98fffd7f87247cbbcf6
Pieter Boersma <pieterboersma20@gmail.com>
2021-01-29 08:52:38 +0100

    Verbeteringen aan het grid

--- src/grid.cpp
+++ src/grid.cpp
@@ -66,14 +66,9 @@ namespace Tmpl8 {
     void Grid::handleAction(Rocket* rocket){
         for(int x = 0; x < Grid::NUM_CELLS; x++){ 
             for(int y = 0; y < Grid::NUM_CELLS; y++){ 
-                int cellX = (int)(rocket->position.x / Grid::CELL_SIZE);
-                int cellY = (int)(rocket->position.y / Grid::CELL_SIZE);
-
                 if(cellX == x && cellY == y){
                     if(this->cells[x][y] != NULL){
-                        if(rocket->allignment != this->cells[x][y]->allignment){
-                            this->handleCell(rocket, x, y);
-                        }
+                        this->handleCell(rocket, x, y); 
                     }
                 }
             }
@@ -85,22 +80,20 @@ namespace Tmpl8 {
         Tank* tank = this->cells[x][y];
         while(tank != NULL){
             this->handleUnit(rocket, tank);
-            // if(x > 0 && y > 0) if(this->handleUnit(rocket, this->cells[x - 1][y - 1])) break;
-            // if(x > 0) if(this->handleUnit(rocket, this->cells[x - 1][y])) break;
-            // if(y > 0) if(this->handleUnit(rocket, this->cells[x][y - 1])) break;
-            // if(x > 0 && y < this->NUM_CELLS - 1) if(this->handleUnit(rocket, this->cells[x - 1][y + 1])) break;
-
+            if((x > 0 && y > 0) && (this->cells[x - 1][y - 1] != NULL)) this->handleUnit(rocket, this->cells[x - 1][y - 1]);
+            if((x > 0) && (this->cells[x - 1][y] != NULL)) this->handleUnit(rocket, this->cells[x - 1][y]);
+            if((y > 0) && (this->cells[x][y - 1] != NULL)) this->handleUnit(rocket, this->cells[x][y - 1]);
+            if((x > 0 && y < this->NUM_CELLS - 1) && (this->cells[x - 1][y + 1] != NULL)) this->handleUnit(rocket, this->cells[x - 1][y + 1]);
             tank = tank->next;
         }
     }
 
     bool Grid::handleUnit(Rocket* rocket, Tank* tank){
-            if (tank->active && (tank->allignment != rocket->allignment) && rocket->intersects(tank->position, tank->collision_radius)){            
+            if (tank->active && rocket->active && (tank->allignment != rocket->allignment) && rocket->intersects(tank->position, tank->collision_radius)){            
                 this->game->add_explosion(tank->position);
                 if (tank->hit(60)) {
                     this->game->add_smoke(tank->position);
                 }
-
                 rocket->active = false;
                 return true;
             }  
@@ -146,13 +139,9 @@ namespace Tmpl8 {
         int counter = 0;
         for(int x = 0; x < Grid::NUM_CELLS; x++){ 
             for(int y = 0; y < Grid::NUM_CELLS; y++){ 
-            int cellX = (int)(tank->get_position().x / Grid::CELL_SIZE);
-            int cellY = (int)(tank->get_position().y / Grid::CELL_SIZE);
                 if(this->cells[x][y] != NULL){
-                    if (this->cells[x][y]->allignment == tank->allignment) {
-                        if(x == cellX && y == cellY){
-                            this->handleCell(tank, x, y);
-                        }
+                    if (tank->get_distance(this->cells[x][y]) < tank->get_collision_radius()) {
+                        this->handleCell(tank, x, y);
                     }
                 }
             }
@@ -181,13 +170,18 @@ namespace Tmpl8 {
         Tank* other = this->cells[x][y];
         while(other != NULL){
             if(tank != other) {
-                this->handleUnit(tank, other);
+                this->handleUnit(tank, other);   
+                if((x > 0 && y > 0) && (this->cells[x - 1][y - 1] != NULL)) this->handleUnit(tank, this->cells[x - 1][y - 1]);
+                if((x > 0) && (this->cells[x - 1][y] != NULL)) this->handleUnit(tank, this->cells[x - 1][y]);
+                if((y > 0) && (this->cells[x][y - 1] != NULL)) this->handleUnit(tank, this->cells[x][y - 1]);
+                if((x > 0 && y < this->NUM_CELLS - 1) && (this->cells[x - 1][y + 1] != NULL)) this->handleUnit(tank, this->cells[x - 1][y + 1]);
             }
             other = other->next;
         }
     }
 
     void Grid::handleUnit(Tank* tank, Tank* other) {         
+        
         vec2 dir = tank->get_position() - other->get_position();
         
         float dirSquaredLen = dir.sqr_length();
@@ -197,6 +191,6 @@ namespace Tmpl8 {
 
         if (dirSquaredLen < colSquaredLen) {
             tank->push(dir.normalized(), 1.f);
-        } 
+        }
     }   
 }
